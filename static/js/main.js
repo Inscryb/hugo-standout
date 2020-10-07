15bf355238a7a9385b752512cd576ab5fa27b7b9
Chris Wharton <674190+CWharton@users.noreply.github.com>
2020-10-06 20:50:52 -0500

    Update

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+// Init AOS
+AOS.init();
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $(".navbar-brand a").css("color", "#fff");
+            $(".top-bar").removeClass("animated-header");
+        } else {
+            $(".navbar-brand a").css("color", "inherit");
+            $(".top-bar").addClass("animated-header");
+        }
+    });
+});
